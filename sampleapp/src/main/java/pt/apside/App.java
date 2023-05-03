852759edc13e00ec3a82364ae6bca4625dc69e12
jpaulobf <jpaulobf@gmail.com>
2023-05-03 13:04:15 +0100

    Initial commit

---
+++ sampleapp/src/main/java/pt/apside/App.java
@@ -0,0 +1,13 @@
+package pt.apside;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
